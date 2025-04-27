b2316d194b7cd169ce9c08a205541952544f0595
NotSuspendedYet <usefulbk@bk.ru>
2025-04-27 16:05:06 +0300

    12

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,7 @@
  * CORS configuration options
  */
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: ['https://floppairmemorygameclient.onrender.com', 'http://localhost:3000'],
   credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11) choke on 204
 };
